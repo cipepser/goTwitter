081f109f24d856feb02867bac082a3d3190ea90a
cipepser <respepic@gmail.com>
2017-07-23 17:15:20 +0900

    :put_litter_in_its_place:

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"net/url"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/ChimeraCoder/anaconda"
 )
@@ -16,96 +11,22 @@ func main() {
 	anaconda.SetConsumerSecret(os.Getenv("TwitterConsumerSecret"))
 	api := anaconda.NewTwitterApi(os.Getenv("TwitterAccessToken"), os.Getenv("TwitterAccessTokenSecret"))
 
-}
+	// フォロー
+	// api.FollowUser("cipepser")
+	// api.FollowUserId(492787827, nil)
 
-// FollowbySupportAcount
-// keywordで検索してでてきたユーザのフォロワーをn人チェックしてフォローする
-// チェック内容は以下。
-// * 未フォロー
-// * descriptionが空欄でない
-// * フォロワー数が100以上
-// * NWワードがdescriptionかな前に含まれている
-func FollowbySupportAcount(keyword string, api *anaconda.TwitterApi, n int, ngs []string) {
-	supportAcounts, _ := api.GetUserSearch(keyword, nil)
+	// フォロー解除
+	// api.UnfollowUserId(492787827)
 
-	if len(supportAcounts) == 0 {
-		return
-	}
+	// ユーザ検索
+	// users, _ := api.GetUsersLookup("cipepser", nil)
+	// for _, u := range users {
+	// 	fmt.Println(u.Id)
+	// }
 
-	c, _ := api.GetFollowersUser(supportAcounts[0].Id, nil)
+	// 検索してフォロー
+	// v := url.Values{}
+	// v.Set("screen_name", "相互")
+	// SearchandFollow("相互 マンガ", api, v)
 
-	for i := 0; i < int(math.Min(float64(n), float64(len(c.Ids)))); i++ {
-		u, _ := api.GetUsersShowById(c.Ids[i], nil)
-		fmt.Print(u.Name, ": ")
-
-		flgNG := false
-		for _, ng := range ngs {
-			if strings.Contains(u.Description, ng) || strings.Contains(u.Name, ng) {
-				flgNG = true
-				break
-			}
-		}
-		if flgNG {
-			fmt.Println("I do not follow.")
-			continue
-		}
-
-		if !u.Following && u.Description != "" && u.FollowersCount > 100 {
-
-			fmt.Println("just followed!")
-			api.FollowUserId(u.Id, nil)
-
-		} else {
-			fmt.Println("I do not follow.")
-		}
-
-	}
-
-}
-
-// SearchandFollow
-// keywordで探したユーザをフォローしていなければフォローする
-// フォロー済みのユーザも出てしまうので微妙。
-func SearchandFollow(keyword string, api *anaconda.TwitterApi, v url.Values) {
-	users, _ := api.GetUserSearch(keyword, v)
-	for _, u := range users {
-		// fmt.Println(string(u.Id) + ":" + u.Description)
-		// fmt.Println(string(u.Id) + ":" + strconv.FormatBool(u.Following))
-		// fmt.Println("-------------------")
-		// fmt.Println(strconv.FormatInt(u.Id, 10) + ":" + strconv.FormatBool(u.Following))
-		// fmt.Println(u.ScreenName)
-		// fmt.Println(u.Name)
-		// fmt.Println(u.Description)
-		// fmt.Println(u.Entities)
-
-		if !u.Following {
-			api.FollowUserId(u.Id, nil)
-		}
-
-	}
-}
-
-func UnfollowNotEachOther(api *anaconda.TwitterApi) {
-	c, _ := api.GetFriendsIds(nil)
-
-	u, _ := api.GetSelf(nil)
-	myId := u.IdStr
-
-	for _, id := range c.Ids {
-		v := url.Values{}
-		v.Set("source_id", myId)
-		v.Set("target_id", strconv.FormatInt(id, 10))
-
-		r, _ := api.GetFriendshipsShow(v)
-
-		fmt.Println("-----------")
-		fmt.Println(r.Relationship.Target.Screen_name)
-		fmt.Println(r.Relationship.Target.Following)
-
-		// リフォローしてくれてない
-		if !r.Relationship.Target.Following {
-			api.UnfollowUserId(id)
-		}
-
-	}
 }
